9dd89bda5f30dd9f64daa3e116a10cbcdcc0e7c0
mason1900 <mw2010a@gmail.com>
2019-05-24 17:56:58 -0400

    better colution for uva 10340

--- Chapter3-All_in_All/all.cpp
+++ Chapter3-All_in_All/all.cpp
@@ -7,14 +7,6 @@
 #include <cstdlib>
 using namespace std;
 
-struct Chr
-{
-	char c;
-	bool bUsed;
-	Chr(char c='\0', bool bUsed = false) : c(c), bUsed(bUsed){}
-};
-
-
 
 int main()
 {
@@ -31,24 +23,18 @@ int main()
 	{
 		int lenS = strlen(s);
 		int lenT = strlen(t);
-		Chr seqT[maxlen] = {};
 		bool isSub = true;
 		if (lenS > lenT) { isSub = false; }
 		else
 		{
-			for (int i = 0; i < lenT; i++)
-			{
-				seqT[i].c = t[i];
-			}
 			int lastPos = -1;
 			for (int i = 0; i < lenS; i++)
 			{
 				bool found = false;
 				for (int j = 0; j < lenT; j++)
 				{
-					if (seqT[j].c == s[i] && !seqT[j].bUsed && j>lastPos)
+					if (t[j] == s[i] && j>lastPos)
 					{
-						seqT[j].bUsed = true;
 						lastPos = j;
 						found = true;
 						break;
